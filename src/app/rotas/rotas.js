4ceb1a20db5c354af3465b22e2197349750cf75e
kielkow <matheuskiel@fiorifer.com.br>
2019-07-15 21:33:44 -0300

    change error return to livro: req.body in rotas.js

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -55,7 +55,7 @@ module.exports = (app) => {
                 return resp.marko(
                     require('../views/livros/form/form.marko'),
                     {
-                        livro: {},
+                        livro: req.body,
                         errosValidacao: erros.array()
                     }
                 )
