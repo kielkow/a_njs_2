7f8a807744cf4ba307926c81914297bd71b2dfa9
kielkow <matheuskiel@fiorifer.com.br>
2019-07-18 21:54:21 -0300

    Fixed error in sessao-autenticacao

--- src/config/sessao-autenticacao.js
+++ src/config/sessao-autenticacao.js
@@ -1,7 +1,7 @@
 const uuid = require('uuid/v4')
 const sessao = require('express-session')
 const passport = require('passport')
-const LocalStrategy = require('passport').Strategy
+const LocalStrategy = require('passport-local').Strategy
 
 const UsuarioDao = require('../app/infra/usuario-dao');
 const db = require('./database');
